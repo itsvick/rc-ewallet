4eba2b3c5bfc30cb027e79c6453aaeefdf12fcd0
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-07-29 00:02:37 +0530

    Issue #ULP-0000 fix: Refactored credential API calls

--- src/app/services/credential/credential.service.ts
+++ src/app/services/credential/credential.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { forkJoin, Observable, of } from 'rxjs';
+import { forkJoin, from, Observable, of } from 'rxjs';
 import { concatMap, map, switchMap } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 import { DataService } from '../data/data-request.service';
@@ -12,6 +12,7 @@ import { AuthConfigService } from 'src/app/authentication/auth-config.service';
 export class CredentialService {
   baseUrl: string;
   private schemas: any[] = [];
+  private schemaIds = {};
   selectedCategory: string = '';
   credentialList = [];
 
@@ -19,7 +20,7 @@ export class CredentialService {
     private readonly dataService: DataService,
     private readonly authService: AuthService,
     private readonly authConfigService: AuthConfigService
-  ) { 
+  ) {
     // this.baseUrl = environment.baseUrl;
     this.baseUrl = this.authConfigService.config.bffUrl;
   }
@@ -31,16 +32,34 @@ export class CredentialService {
     return false;
   }
 
+  private findSchemaId(credentialId: string) {
+    if (Object.keys(this.schemaIds).length && this.schemaIds[credentialId]) {
+      return this.schemaIds[credentialId];
+    }
+    return false;
+  }
+
   getCredentialSchemaId(credentialId: string): Observable<any> {
+    const schemaId = this.findSchemaId(credentialId);
+    if (schemaId) {
+      return of({ credential_schema: schemaId });
+    }
+
     const payload = { url: `${this.baseUrl}/v1/sso/student/credentials/schema/${credentialId}` };
-    return this.dataService.get(payload).pipe(map((res: any) => res.result));
+    return this.dataService.get(payload).pipe(map((res: any) => {
+      const schemaId = this.findSchemaId(res.result?.credential_schema);
+      if (!schemaId) {
+        this.schemaIds[credentialId] = res.result?.credential_schema;
+      }
+      return res.result;
+    }));
   }
 
   getCredentials(): Observable<any> {
     const payload = {
       url: `${this.baseUrl}/v1/sso/student/credentials/search/student`,
       data: {
-        subject: { id: this.authService.currentUser?.did}
+        subject: { id: this.authService.currentUser?.did }
       }
     };
 
@@ -74,16 +93,41 @@ export class CredentialService {
                 concatMap((res: any) => {
                   console.log("res", res);
                   cred.schemaId = res.credential_schema;
-                  return this.getSchema(res.credential_schema).pipe(
-                    map((schema: any) => {
-                      cred.credential_schema = {...schema};
-                      return cred;
-                    })
-                  );
+                  return of(cred);
+                  // return this.getSchema(res.credential_schema).pipe(
+                  //   map((schema: any) => {
+                  //     cred.credential_schema = {...schema};
+                  //     return cred;
+                  //   })
+                  // );
                 })
               );
             })
           );
+        }
+        return of([]);
+      }), switchMap((res: any) => {
+        console.log("res", res);
+        const schemaIds = [...new Set(res.map((item: any) => item.schemaId))];
+        return from(schemaIds).pipe(
+          switchMap((schemaId: any) => {
+            return this.getSchema(schemaId);
+          }), switchMap((schema: any) => {
+            return of(res);
+          })
+        );
+        // return of(res);
+      }),
+      switchMap((creds: any) => {
+        if (creds.length) {
+          return forkJoin(creds.map((cred: any) => {
+            return this.getSchema(cred.schemaId).pipe(
+              map((schema: any) => {
+                cred.credential_schema = { ...schema };
+                return cred;
+              })
+            );
+          }))
         }
         return of([]);
       })
