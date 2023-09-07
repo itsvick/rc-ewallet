3164a1ed0c6a701d4474011b1f6dc576e3482abf
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-09-07 16:26:29 +0530

    Issue #ULP-0000 feat: Added telemetry Events

--- src/app/aadhar-kyc/aadhar-kyc.component.ts
+++ src/app/aadhar-kyc/aadhar-kyc.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 import { HttpHeaders } from '@angular/common/http';
 import { AuthConfigService } from '../authentication/auth-config.service';
@@ -8,6 +8,8 @@ import { DataService } from '../services/data/data-request.service';
 import { map } from 'rxjs/operators';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AadhaarKycStatusComponent } from '../aadhaar-kyc-status/aadhaar-kyc-status.component';
+import { IInteractEventInput } from '../services/telemetry/telemetry-interface';
+import { TelemetryService } from '../services/telemetry/telemetry.service';
 
 @Component({
   selector: 'app-aadhar-kyc',
@@ -34,7 +36,9 @@ export class AadharKycComponent implements OnInit {
     private readonly router: Router,
     private readonly authConfigService: AuthConfigService,
     private readonly dataService: DataService,
-    private readonly modalService: NgbModal
+    private readonly modalService: NgbModal,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly telemetryService: TelemetryService
   ) { }
 
   ngOnInit(): void {
@@ -48,10 +52,12 @@ export class AadharKycComponent implements OnInit {
   }
 
   getOTP() {
+    this.raiseInteractEvent('aadhaar-kyc-get-otp');
     this.isGetOTPClicked = true;
   }
 
   submitOTP() {
+    this.raiseInteractEvent('aadhaar-kyc-submit-otp');
     const payload = {
       aadhaar_id: this.aadhaarFormControl.value,
     }
@@ -86,6 +92,7 @@ export class AadharKycComponent implements OnInit {
 
   startKYC() {
     this.showForm = true;
+    this.raiseInteractEvent('start-kyc');
   }
 
   openStatusModal() {
@@ -97,13 +104,32 @@ export class AadharKycComponent implements OnInit {
 
     modalRef.componentInstance.isVerified = this.isAadhaarVerified;
     modalRef.componentInstance.tryAgain.subscribe(() => {
+      this.raiseInteractEvent('aadhaar-kyc-try-again');
       modalRef.dismiss();
     });
 
     modalRef.componentInstance.close.subscribe(() => {
+      this.raiseInteractEvent('aadhaar-kyc-success-goto-wallet');
       this.kycCompleted.emit(true);
       modalRef.dismiss();
     });
   }
+
+  raiseInteractEvent(id: string, type: string = 'CLICK', subtype?: string) {
+    const telemetryInteract: IInteractEventInput = {
+      context: {
+        env: this.activatedRoute.snapshot?.data?.telemetry?.env,
+        cdata: []
+      },
+      edata: {
+        id,
+        type,
+        subtype,
+        pageid: this.activatedRoute.snapshot?.data?.telemetry?.pageid,
+      }
+    };
+    this.telemetryService.interact(telemetryInteract);
+  }
+
 
 }
