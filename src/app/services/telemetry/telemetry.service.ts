c2904a56d8cdc468790834319f261cb8d86060d9
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-09-13 10:52:19 +0530

    Issue #ULP-889 feat: Updated API's endpoints

--- src/app/services/telemetry/telemetry.service.ts
+++ src/app/services/telemetry/telemetry.service.ts
@@ -55,7 +55,7 @@ export class TelemetryService {
         authtoken: this.context.authToken || '',
         uid: this.context.uid || '',
         sid: this.context.sid,
-        batchsize: 2,
+        batchsize: 20,
         mode: this.context.mode,
         host: this.context.host,
         endpoint: this.context.endpoint || '/v1/telemetry',
